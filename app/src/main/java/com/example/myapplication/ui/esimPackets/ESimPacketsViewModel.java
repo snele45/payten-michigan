dce73ba95ff60d4021f3f4c0ad66ae773918f957
Nemanja Andrić <nandric00@gmail.com>
2024-12-01 13:42:58 +0100

    eSIM packages screen

---
+++ app/src/main/java/com/example/myapplication/ui/esimPackets/ESimPacketsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.esimPackets;
+
+import androidx.lifecycle.ViewModel;
+
+public class ESimPacketsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
