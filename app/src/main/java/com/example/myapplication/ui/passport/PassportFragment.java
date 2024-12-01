8351fb95c3cbb17a32301c23fec58e030199f637
Stefan Neskovic <sneskovic4@hotmail.com>
2024-12-01 17:58:43 +0100

    payment back to main after

--- app/src/main/java/com/example/myapplication/ui/passport/PassportFragment.java
+++ app/src/main/java/com/example/myapplication/ui/passport/PassportFragment.java
@@ -3,19 +3,25 @@ package com.example.myapplication.ui.passport;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+
+import java.math.BigDecimal;
 
 public class PassportFragment extends Fragment {
 
@@ -39,7 +45,7 @@ public class PassportFragment extends Fragment {
         if (context != null) {
             nfcAdapter = NfcAdapter.getDefaultAdapter(context);
             if (nfcAdapter == null) {
-              //  Toast.makeText(context, "NFC is not available on this device.", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "NFC is not available on this device.", Toast.LENGTH_LONG).show();
             } else if (!nfcAdapter.isEnabled()) {
                 Toast.makeText(context, "NFC is disabled.", Toast.LENGTH_LONG).show();
             }
@@ -49,7 +55,7 @@ public class PassportFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        handler.postDelayed(this::simulateNfcTagRead, 5000); // Delay of 10 seconds
+        handler.postDelayed(this::simulateNfcTagRead, 5000); // Delay of 5 seconds
     }
 
     @Override
@@ -60,11 +66,42 @@ public class PassportFragment extends Fragment {
 
     private void showNfcReadSuccessDialog() {
         if (getActivity() != null) {
-            new AlertDialog.Builder(getActivity())
-                    .setTitle("NFC Scan")
-                    .setMessage("Podaci uspešno očitani.")
-                    .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
-                    .show();
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.dialog_nfc_scan, null);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setView(dialogView);
+
+            // References for inputs and icon
+            ImageView tickIcon = dialogView.findViewById(R.id.tickIcon);
+            EditText inputFullName = dialogView.findViewById(R.id.inputFullName);
+            EditText inputCitizenNumber = dialogView.findViewById(R.id.inputCitizenNumber);
+            EditText inputAddress = dialogView.findViewById(R.id.inputAddress);
+            Button buttonContinue = dialogView.findViewById(R.id.buttonContinue);
+
+            // Set default values for inputs
+            inputFullName.setText("Stefan Neskovic");
+            inputCitizenNumber.setText("1110000790013");
+            inputAddress.setText("Podgradina 33, Kosjeric");
+
+            // Disable editing for inputs
+            inputFullName.setEnabled(false);
+            inputCitizenNumber.setEnabled(false);
+            inputAddress.setEnabled(false);
+
+            AlertDialog dialog = builder.create();
+
+            buttonContinue.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    BigDecimal bd = new BigDecimal(1190);
+                    ((MainActivity)getActivity()).performPayment( bd );
+                }
+                //    NavController navController = Navigation.findNavController(v);
+                //  navController.navigate(R.id.action_nav_insurancePackages_to_nav_travelInfo);
+            });
+
+            dialog.show();
         }
     }
 
